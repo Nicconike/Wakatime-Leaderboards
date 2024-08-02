1fe82b8a7b49cba64c3fe429204b36ab8d0520da
semantic-release <semantic-release>
2024-08-02 21:47:38 +0000

    chore(release): Version Release 0.0.0

--- api/main.py
+++ api/main.py
@@ -19,7 +19,7 @@ WAKATIME_API_KEY = os.environ["INPUT_WAKATIME_API_KEY"]
 REQUEST_TIMEOUT = (25, 30)
 
 # Version Identifier for Changelog
-__version__ = "1.0.0"
+__version__ = "0.0.0"
 
 
 def format_time(seconds):
